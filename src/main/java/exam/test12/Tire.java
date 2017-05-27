5b1ffe205bdb2a98b1620a782f1c99c1a5e8062e
Dongho Sim <dhsim86@gmail.com>
2017-05-27 20:24:10 +0900

    [ch08] 8.3 Use factory bean.

---
+++ src/main/java/exam/test12/Tire.java
@@ -0,0 +1,51 @@
+package exam.test12;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Created by nhnent on 2017. 5. 27..
+ */
+public class Tire {
+	private String maker;
+	private Properties spec;
+	private Date createdDate;
+
+	public String getMaker() {
+		return maker;
+	}
+
+	public void setMaker(String maker) {
+		this.maker = maker;
+	}
+
+	public Properties getSpec() {
+		return spec;
+	}
+
+	public void setSpec(Properties spec) {
+		this.spec = spec;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer specInfo = new StringBuffer();
+
+		if (spec != null) {
+			for (Map.Entry<Object, Object> entry : spec.entrySet()) {
+				specInfo.append(entry.getKey() + ":" + entry.getValue() + ", ");
+			}
+		}
+
+		return "[Tire: " + maker + ", " + specInfo.toString() + ((createdDate != null) ? (", " + createdDate.toString()) : "") + "]";
+	}
+}
